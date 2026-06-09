8f088dc56436fdcb616b0b4beb58d2846ac05fce
AnaCKrause561 <dasilvaanacristina199@gmail.com>
2026-06-08 22:09:16 -0300

    Agenda_login

---
+++ Servidor e Prim. PHP/WebServer/Apache24/htdocs/Agenda - Avaliação/app/views/dashboard.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard</title>
+    <link href="../../public/css/style_dashboard.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+    <div class="container">
+
+        <!-- MENU -->
+        <aside class="menu_esquerda">
+            <h2>Agenda</h2>
+            
+            <ul>
+                <li></li>
+
+    </div>
+</body>
+</html>
